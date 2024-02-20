216b96a85395a43fb0ec811efe68aa010d49816c
LAPTOP-84OVSUUU\u_arg <u_argun@skenariolabs.com>
2024-02-21 01:35:06 +0200

    changed the commentout for multiple lines and corrected the typo

--- letterCounter.py
+++ letterCounter.py
@@ -1,10 +1,10 @@
-#               Mini project
-# 1 - Print a welcome message to the user
-# 2 - Take a string (message) and a letter from the user
-# 3 - Count hpw many times this letter occurred
-# 4 - Calculate the percentage of the letter in the message
-# 5 - Print the output to the user
-
+"""               Mini project
+ 1 - Print a welcome message to the user
+ 2 - Take a string (message) and a letter from the user
+ 3 - Count how many times this letter occurred
+ 4 - Calculate the percentage of the letter in the message
+ 5 - Print the output to the user
+"""
 # Welcome message
 print("Welcome to our mini project!")
 # Take the user inputs
