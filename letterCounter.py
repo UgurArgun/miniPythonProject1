a02f115d54fd5885ed19ac7acfa41c7ff10420e1
LAPTOP-84OVSUUU\u_arg <u_argun@skenariolabs.com>
2024-02-21 01:28:04 +0200

    added a basic program for user inputs, count and calculate percentage of a letter in letters

---
+++ letterCounter.py
@@ -0,0 +1,26 @@
+#               Mini project
+# 1 - Print a welcome message to the user
+# 2 - Take a string (message) and a letter from the user
+# 3 - Count hpw many times this letter occurred
+# 4 - Calculate the percentage of the letter in the message
+# 5 - Print the output to the user
+
+# Welcome message
+print("Welcome to our mini project!")
+# Take the user inputs
+user_message = input("Please enter your message! ")
+user_letter = input("Please enter the letter ")
+
+# count the occurrences
+count = user_message.count(user_letter)
+print(count)
+
+# Calculate the percentage
+len_message = len(user_message)
+print(len_message)
+percentage = count/len_message*100
+print(percentage)
+
+# Print the output to the user
+print("The count of the letter", user_letter , "is", count)
+print("The percentage of the letter", user_letter, "is", percentage)
